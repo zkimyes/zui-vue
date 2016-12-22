0bd288418d5a06ee5ba112d4e29a92affdad6312
153033541@qq.com <153033541@qq.com>
2016-12-22 16:44:52 +0800

    ...

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,28 @@
 <template>
   <div id="app">
-      <div class="box1">
-        <div v-for="i in bdata">{{i}}</div>
-      </div>
-      <tab>
-        <tab-item :selected="demo1 === '已发货'" @click="demo1 = '已发货'">已发货</tab-item>
-        <tab-item :selected="demo1 === '未发货'" @click="demo1 = '未发货'">未发货</tab-item>
-        <tab-item :selected="demo1 === '全部订单'" @click="demo1 = '全部订单'">全部订单</tab-item>
-      </tab>
+    <img src="./assets/logo.png">
+    <hello></hello>
   </div>
 </template>
 
 <script>
-    import scroller from 'vux/src/components/scroller'
-    import Tab from 'vux/src/components/tab'
+import Hello from './components/Hello'
 
-    export default {
-        name: 'app',
-        components: {
-            Tab,
-            scroller
-        },
-        data() {
-            return {
-                bdata: [1, 2, 3, 4, 5, 6, 7],
-                demo1: '已发货'
-            }
-        }
-    }
+export default {
+  name: 'app',
+  components: {
+    Hello
+  }
+}
 </script>
 
 <style>
-    #app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-        margin-top: 60px;
-    }
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
