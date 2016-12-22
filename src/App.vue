7ac38752ac726105927e7968d60eb3c66d04f66b
153033541@qq.com <153033541@qq.com>
2016-12-22 16:12:33 +0800

    build project with vue-cli

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+      <div class="box1">
+        <div v-for="i in bdata">{{i}}</div>
+      </div>
+      <tab>
+        <tab-item :selected="demo1 === '已发货'" @click="demo1 = '已发货'">已发货</tab-item>
+        <tab-item :selected="demo1 === '未发货'" @click="demo1 = '未发货'">未发货</tab-item>
+        <tab-item :selected="demo1 === '全部订单'" @click="demo1 = '全部订单'">全部订单</tab-item>
+      </tab>
+  </div>
+</template>
+
+<script>
+    import scroller from 'vux/src/components/scroller'
+    import Tab from 'vux/src/components/tab'
+
+    export default {
+        name: 'app',
+        components: {
+            Tab,
+            scroller
+        },
+        data() {
+            return {
+                bdata: [1, 2, 3, 4, 5, 6, 7],
+                demo1: '已发货'
+            }
+        }
+    }
+</script>
+
+<style>
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+</style>
